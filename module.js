3f61984dd7a45a877124e9045661a5d628a6fbbb
Vo Van Hoang Tuan <vovanhoangtuan4.2@gmail.com>
2019-06-10 12:15:32 +0700

    Chang File Module.js

--- module.js
+++ module.js
@@ -14,11 +14,10 @@ async function tyPing(dataFb, receiver){
 		method: 'post',
 		data: formData,
 		headers: {
-			'accept': '*/*',
-			'accept-encoding': 'gzip, deflate, sdch',
-			'accept-language': 'en-US,en;q=0.8,en-AU;q=0.6',
-			'cookie': dataFb.cookie,
-		// 'dnt': '1',
+		'accept': '*/*',
+		'accept-encoding': 'gzip, deflate, sdch',
+		'accept-language': 'en-US,en;q=0.8,en-AU;q=0.6',
+		'cookie': dataFb.cookie,
 		'origin': 'https://www.facebook.com',
 		'referer': 'https://www.facebook.com/',
 		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
